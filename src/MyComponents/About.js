f1c4fcd6c9b8f09ee64e6e60629dc129a38cbc2e
Naman Gupta <guptanaman011@gmail.com>
2021-05-27 14:48:57 +0530

    commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is  my about component
+            <p>Lorem ipsum dolor sit ame consectitur, adipisicing
+            </p>
+        </div>
+    )
+}
